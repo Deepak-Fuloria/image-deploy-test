dc6dc783aa4fd004fff6dcdd1a532346cd652c21
Deepak Fuloria <frontenddeveloperintern1@gmail.com>
2023-08-10 16:33:03 +0530

    m1

--- controller/controller.js
+++ controller/controller.js
@@ -18,27 +18,57 @@ const showPage = async (req, res) => {
 }
 
 
-const uploadImage = (req, res) => {
+// const uploadImage = (req, res) => {
 
-    var form = new formidable.IncomingForm();
+//     var form = new formidable.IncomingForm();
+//     uploadDir: path.join(__dirname, '../public', 'images', 'temp'),
+//         form.parse(req, async (err, fields, files) => {
+
+//             var oldpath = files.image[0].filepath;
+//             var newpath = path.join(__dirname, '../public', 'images', files.image[0].originalFilename);
+
+//             fs.rename(oldpath, newpath, function (err) {
+
+//                 if (err) throw err;
+//                 res.end();
+//             });
+
+
+//             const test = new Test({ imageName: files.image[0].originalFilename })
+//             test.save()
+
+//             res.redirect('/')
+
+//         });
+// }
+
+const uploadImage = (req, res) => {
+    const form = new formidable.IncomingForm({
+        uploadDir: path.join(__dirname, '../public', 'images', 'temp'),
+    });
+
     form.parse(req, async (err, fields, files) => {
+        if (err) {
+            console.error('Form parsing error:', err);
+            return res.status(500).send('Error while processing form.');
+        }
 
         var oldpath = files.image[0].filepath;
-        var newpath = path.join(__dirname, '../public', 'images', files.image[0].originalFilename);
+        const newpath = path.join(__dirname, '../public', 'images', files.image[0].originalFilename);
 
         fs.rename(oldpath, newpath, function (err) {
-            if (err) throw err;
-            res.end();
+            if (err) {
+                console.error('File rename error:', err);
+                return res.status(500).send('Error while renaming file.');
+            }
+
+            const test = new Test({ imageName: files.image[0].originalFilename });
+            test.save()
+            res.redirect('/');
         });
-
-
-        const test = new Test({ imageName: files.image[0].originalFilename })
-        test.save()
-
-        res.redirect('/')
-
     });
-}
+};
+
 
 
 
